fb9b2b4745fa1299c5e63e789c5d96fd80571d63
rodriguezny <rodrigueznyiringabo@gmail.com>
2020-09-16 19:01:59 +0100

    Middleware d'authentification par token et par API_key version 2

--- routes/utilisateurs.js
+++ routes/utilisateurs.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import utilisateurCtrl from '../controllers/utilisateurCtrl';
 import authToken from '../middlewares/authToken';
-import auth from '../middlewares/authToken';
+import auth_API_key from '../middlewares/auth_API_key';
 
 
 const router = express.Router();
 
 router.post("/login", utilisateurCtrl.login)
-      .post("/user", auth_API-key, authToken, utilisateurCtrl.saveUser)
-      .delete("/user", auth_API-key, authToken, utilisateurCtrl.deleteUser)
-      .put("/user", auth_API-key,authToken, utilisateurCtrl.modifyUser);
+      .post("/user", auth_API_key, authToken, utilisateurCtrl.saveUser)
+      .delete("/user", auth_API_key, authToken, utilisateurCtrl.deleteUser)
+      .put("/user", auth_API_key,authToken, utilisateurCtrl.modifyUser);
 
 
 export default router;
